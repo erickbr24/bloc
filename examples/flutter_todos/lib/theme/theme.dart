808e4e45da84ae19b904f3b02a0d05c11b1aaf50
erickbr24 <erickbr24@gmail.com>
2023-07-17 21:52:28 -0400

    initial state for further practice with bloc

--- examples/flutter_todos/lib/theme/theme.dart
+++ examples/flutter_todos/lib/theme/theme.dart
@@ -4,10 +4,10 @@ class FlutterTodosTheme {
   static ThemeData get light {
     return ThemeData(
       appBarTheme: const AppBarTheme(
-        color: Color(0xFF13B9FF),
+        color: Color.fromARGB(255, 224, 151, 13),
       ),
       colorScheme: ColorScheme.fromSwatch(
-        accentColor: const Color(0xFF13B9FF),
+        accentColor: Color.fromARGB(255, 224, 151, 13),
       ),
       snackBarTheme: const SnackBarThemeData(
         behavior: SnackBarBehavior.floating,
@@ -18,11 +18,11 @@ class FlutterTodosTheme {
   static ThemeData get dark {
     return ThemeData(
       appBarTheme: const AppBarTheme(
-        color: Color(0xFF13B9FF),
+        color: Color.fromARGB(255, 53, 0, 78),
       ),
       colorScheme: ColorScheme.fromSwatch(
         brightness: Brightness.dark,
-        accentColor: const Color(0xFF13B9FF),
+        accentColor: Color.fromARGB(255, 53, 0, 78),
       ),
       snackBarTheme: const SnackBarThemeData(
         behavior: SnackBarBehavior.floating,
